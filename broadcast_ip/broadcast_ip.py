fe05fee692ace2f90d60a7a0a5823a3aed88d1b9
andyowenx <andyowenx@gmail.com>
2018-05-28 03:59:46 +0000

    update program

--- broadcast_ip/broadcast_ip.py
+++ broadcast_ip/broadcast_ip.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python 
 import time
+import daemon
 from socket import *
 
-fd = socket(AF_INET, SOCK_DGRAM)  
-fd.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  
-fd.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  
+def broadcast():
+    fd = socket(AF_INET, SOCK_DGRAM)  
+    fd.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  
+    fd.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  
+    data = "Hello world, this is Pi"
 
-data = "Hello world"
+    while 1:
+	fd.sendto(data, ('255.255.255.255', 50022))
+        time.sleep(30)
 
-while 1:
-    fd.sendto(data, ('255.255.255.255', 50022))
-    time.sleep(30)
-
+if __name__ == "__main__":
+    with daemon.DaemonContext():
+	broadcast()
 
