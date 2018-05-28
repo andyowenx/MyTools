2c02e3845bbe2984a8fb94d16ef1bf53c3a74f1b
andyowenx <andyowenx@gmail.com>
2018-05-28 02:26:34 +0000

    Broadcast IP tool

---
+++ broadcast_ip/broadcast_ip.py
@@ -0,0 +1,15 @@
+import time
+from socket import *
+import netifaces as ni
+
+fd = socket(AF_INET, SOCK_DGRAM)  
+fd.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  
+fd.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  
+
+data = "Hello world"
+
+while 1:
+    fd.sendto(data, ('255.255.255.255', 50022))
+    time.sleep(30)
+
+
